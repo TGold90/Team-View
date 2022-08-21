d370dc6ae0cdad0c2c05a1fde6edb3546be64f45
TGOLD <thomaswgold90@gmail.com>
2022-08-20 18:36:27 -0600

    finished html templates

--- utils/genHTML.js
+++ utils/genHTML.js
@@ -2,21 +2,76 @@ const genHTML = (data) => {
   const manager = (arr) => {
     const managerText = [];
     arr.forEach((man) => {
-      managerText.push(`<div> name: ${man.name} </div>`);
+      managerText.push(`<div class="card manager column is-3 m-2">
+      <header class="card-header role-header">
+        <p class="card-header-title">Manager: ${man.name}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <p>ID: ${man.id}</p>
+          <p>
+            Email:
+            <a href="mailto: ${man.email}" target="_blank"
+              >${man.email}</a
+            >
+          </p>
+          <footer class="card-footer pt-3">
+            <p>Office Number: ${man.officeNumber}</p>
+          </footer>
+        </div>
+      </div>
+    </div>`);
     });
     return managerText.join("");
   };
   const intern = (arr) => {
     const internText = [];
     arr.forEach((int) => {
-      internText.push(`<div> name: ${int.name} </div>`);
+      internText.push(`<div class="card intern column is-3 m-2">
+      <header class="card-header role-header">
+        <p class="card-header-title">Intern: ${int.name}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <p>ID: ${int.id}</p>
+          <p>
+            Email:
+            <a href="mailto: ${int.email}" target="_blank"
+              >${int.email}</a
+            >
+          </p>
+          <footer class="card-footer pt-3">
+            <p>School: ${int.school}</p>
+          </footer>
+        </div>
+      </div>
+    </div>`);
     });
     return internText.join("");
   };
   const engineer = (arr) => {
     const engineerText = [];
     arr.forEach((eng) => {
-      engineerText.push(`<div> name: ${eng.name} </div>`);
+      engineerText.push(`<div class="card engineer column is-3 m-2">
+      <header class="card-header role-header">
+        <p class="card-header-title">Engineer: ${eng.name}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <p>ID: ${eng.id}</p>
+          <p>
+            Email:
+            <a href="mailto: ${eng.email}" target="_blank"
+              >${eng.email}</a
+            >
+          </p>
+          <footer class="card-footer pt-3">
+            <p>GitHub: <a href="https://github.com/${eng.gitHub}" target="_blank"
+              >${eng.gitHub}</a</p>
+          </footer>
+        </div>
+      </div>
+    </div>`);
     });
     return engineerText.join("");
   };
