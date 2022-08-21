de33d3263a85cf793e21d450e6faee61b51dbdb9
TGOLD <thomaswgold90@gmail.com>
2022-08-20 19:34:03 -0600

    styling

--- utils/genHTML.js
+++ utils/genHTML.js
@@ -3,8 +3,13 @@ const genHTML = (data) => {
     const managerText = [];
     arr.forEach((man) => {
       managerText.push(`<div class="card manager column is-3 m-2">
-      <header class="card-header role-header">
-        <p class="card-header-title">Manager: ${man.name}</p>
+      <header class="card-header role-header man-header">
+        <p class="card-header-title headTitle">Manager: ${man.name}</p>
+        <button class="card-header-icon" aria-label="dragon">
+                <span class="icon">
+                  <i class="fas fa-dragon"></i>
+                </span>
+              </button>
       </header>
       <div class="card-content">
         <div class="content">
@@ -29,7 +34,12 @@ const genHTML = (data) => {
     arr.forEach((int) => {
       internText.push(`<div class="card intern column is-3 m-2">
       <header class="card-header role-header">
-        <p class="card-header-title">Intern: ${int.name}</p>
+        <p class="card-header-title headTitle">Intern: ${int.name}</p>
+        <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-edit"></i>
+                </span>
+              </button>
       </header>
       <div class="card-content">
         <div class="content">
@@ -53,8 +63,13 @@ const genHTML = (data) => {
     const engineerText = [];
     arr.forEach((eng) => {
       engineerText.push(`<div class="card engineer column is-3 m-2">
-      <header class="card-header role-header">
-        <p class="card-header-title">Engineer: ${eng.name}</p>
+      <header class="card-header role-header eng-header">
+        <p class="card-header-title headTitle">Engineer: ${eng.name}</p>
+        <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-file-code"></i>
+                </span>
+              </button>
       </header>
       <div class="card-content">
         <div class="content">
@@ -62,12 +77,12 @@ const genHTML = (data) => {
           <p>
             Email:
             <a href="mailto: ${eng.email}" target="_blank"
-              >${eng.email}</a
-            >
+              >${eng.email}</a>
+            
           </p>
           <footer class="card-footer pt-3">
             <p>GitHub: <a href="https://github.com/${eng.gitHub}" target="_blank"
-              >${eng.gitHub}</a</p>
+              >${eng.gitHub}</a>
           </footer>
         </div>
       </div>
@@ -76,19 +91,35 @@ const genHTML = (data) => {
     return engineerText.join("");
   };
   return `<!DOCTYPE html>
-    <html lang="en">
+  <html>
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team View</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Team View</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+      />
+      <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+    />
+      <link rel="stylesheet" href="/dist/style.css" />
     </head>
     <body>
+      <section class="section">
+        <div class="container">
+          <h1 class="title">Team View</h1>
+          <p class="subtitle">Org chart <strong>Visualized!</strong></p>
+          <div class="columns is-flex flex-wrap">
         ${manager(data.filter((member) => member.getRole() === "Manager"))}
         ${engineer(data.filter((member) => member.getRole() === "Engineer"))}
         ${intern(data.filter((member) => member.getRole() === "Intern"))}
+        </div>
+        </div>
+      </section>
     </body>
-    </html>`;
+  </html>`;
 };
 
 module.exports = genHTML;
